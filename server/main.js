fff9471ed64b8693e8f4153f97981887d19d7850
Victor Akerele <eneroakerele@gmail.com>
2018-05-01 11:45:15 +0100

    setup cloudinary

--- server/main.js
+++ server/main.js
@@ -2,4 +2,9 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  Cloudinary.config({
+    cloud_name: 'divk5nutg',
+    api_key: '563392565759232',
+    api_secret: 'UsPa5uDyP02a2oHcCQDB9ftLkTI'
+  });
 });
