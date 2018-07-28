5a4ffa0a0ca966c18b1a442f5d459adbbd0d4b68
victor <eneroakerele@gmail.com>
2018-07-28 21:08:18 +0100

    dynamic states vis country

--- client/controllers/checkout.js
+++ client/controllers/checkout.js
@@ -1,4 +1,35 @@
 import plugins from '../plugins';
+import country from './../countries.json'
+import states from './../states.json'
+
+Template.checkout.onCreated( function(){
+  this.country = new ReactiveVar("Nigeria");
+})
+
 Template.checkout.onRendered( function(){
   plugins()
+})
+
+Template.checkout.helpers({
+  country: function(){
+    return states;    
+  },
+  state: function(){
+    args = Template.instance().country.get();
+    let res = states.filter( function(val){
+      if(val.country === args){
+        return val;
+      }
+    });
+    console.log(res)
+    return res[0].states;
+  }
+})
+
+Template.checkout.events({
+  "change #country":function(event, template){
+    console.log("changed");
+    let country = event.currentTarget.value;
+    template.country.set(country);
+  }
 })
