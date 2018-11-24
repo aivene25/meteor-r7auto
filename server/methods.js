0a73123c7ecc0b56b9869f6734425335a90536d5
victor <eneroakerele@gmail.com>
2018-11-24 13:40:09 +0100

    toggle cars visibiilty

--- server/methods.js
+++ server/methods.js
@@ -1,9 +1,9 @@
 Meteor.methods({
   sendMail: (to, subject, html) => {
     check([to, subject, html], [String]);
-    // verify if this.ublock is a function
-    //this.unblock();
     const from = "R7Auto <info.r7auto@gmail.com>";
-    Email.send({ to,from,subject, html });
+    Meteor.defer(() => {
+      Email.send({ to, from, subject, html });
+    });
   }
 });
--- server/publications.js
+++ server/publications.js
@@ -1,16 +1,16 @@
-Meteor.publish('posts.all', function(){
-    return Posts.find({}, {});
-})
-Meteor.publish('services.all', function(){
-    return Services.find({}, {});
-})
+Meteor.publish("posts.all", function() {
+  return Posts.find();
+});
+Meteor.publish("services.all", function() {
+  return Services.find();
+});
 
-Meteor.publish('car-services.all', function(){
-    return CarServicing.find({}, {});
-})
-Meteor.publish('cars.all', function(){
-    return Cars.find({}, {});
-})
-Meteor.publish('parts.all', function(){
-    return SpareParts.find({}, {});
-})
+Meteor.publish("car-services.all", function() {
+  return CarServicing.find();
+});
+Meteor.publish("cars.all", function() {
+  return Cars.find({ show: true });
+});
+Meteor.publish("parts.all", function() {
+  return SpareParts.find();
+});
